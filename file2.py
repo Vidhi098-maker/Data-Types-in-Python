b9a37ab450bcd6d7e613bb12a64c18654c2c4617
Vidhi098-maker <devanwaza@gmail.com>
2026-04-08 18:49:16 +0100

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,18 @@
+a = 3
+print(type(a))
+b = 0.2
+print(type(b))
+c = "Hello"
+print(type(c))
+d = True
+print(type(d))
+
+#converting integer to string
+
+x = str(a)
+print(type(x))
+
+#converting float to int
+
+y = int(b)
+print(type(y))
---
+++ file3.py
@@ -0,0 +1,4 @@
+x = str(input("Enter a string"))
+y = x [::-1]
+x = y
+print("The reversed string is",x)
