cc3afb0fac2f8525224d59ce86bab9c664e77b6d
Vidhi098-maker <devanwaza@gmail.com>
2026-04-09 12:34:07 +0100

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,10 @@
+code = "Congratulations"
+print(code.upper())
+
+code = "Congratulations"
+print(code.lower())
+
+message = "Congratulations"
+print(message)
+tokens = message.split()
+print(tokens)
